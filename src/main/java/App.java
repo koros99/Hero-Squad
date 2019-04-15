f2ec6952dac379cb2750a462c1d420a68507b41d
koros99 <koronje254@gmail.com>
2019-04-15 04:45:37 +0300

    Add route for viewing each hero's information

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -36,13 +36,13 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-//    get("/heroes/:id", (request, response) -> {
-//      Map<String, Object> model = new HashMap<String, Object>();
-//      Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
-//      model.put("hero", hero);
-//      model.put("template", "templates/hero.vtl");
-//      return new ModelAndView(model, layout);
-//    }, new VelocityTemplateEngine());
+    get("/heroes/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
+      model.put("hero", hero);
+      model.put("template", "templates/hero.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 //
 //
 //
