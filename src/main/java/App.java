93215c1b8877cb9b4c5d3dd14a9d772494651d0d
koros99 <koronje254@gmail.com>
2019-04-15 05:02:54 +0300

    Add routes to swiitch from squads to heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -66,22 +66,22 @@ public class App {
       model.put("template", "templates/squads.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-//
-//    get("/squads/:id", (request, response) -> {
-//      Map<String, Object> model = new HashMap<String, Object>();
-//      Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
-//      model.put("squad", squad);
-//      model.put("template", "templates/squad.vtl");
-//      return new ModelAndView(model, layout);
-//    }, new VelocityTemplateEngine());
-//
-//    get("squads/:id/heroes/new", (request, response) -> {
-//      Map<String, Object> model = new HashMap<String, Object>();
-//      Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
-//      model.put("squad", squad);
-//      model.put("template", "templates/squad-heroes-form.vtl");
-//      return new ModelAndView(model, layout);
-//    }, new VelocityTemplateEngine());
+
+    get("/squads/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+      model.put("squad", squad);
+      model.put("template", "templates/squad.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("squads/:id/heroes/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+      model.put("squad", squad);
+      model.put("template", "templates/squad-heroes-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 //
     post("/heroes", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
